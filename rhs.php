b613a0cd44b8669cfd59f24379c48381724157c3
Sriram_R <sriram@ubuntu.ubuntu-domain>
2011-06-01 22:28:34 +0530

    You can now 'FB Like'!

--- rhs.php
+++ rhs.php
@@ -10,4 +10,7 @@
         <br/><br/>
         I'm a rider from the South and I hand craft web based software to make a living. What started as a personal diary of memorable travels, became a public blog and eventually encouraged me to ride all over India. Visit <a href="http://www.praveenselvam.com">my website</a> for more info. I've also completed a <a href="http://www.ironbutt.com/ridecerts/getdocument.cfm?DocID=30" target="_blank">Saddle Sore 1600K</a> on my Ninja, but haven't registered it with the IBA.
     </div>
+    <div id="fblike">
+    <iframe src="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FRoad-to-ST7-Praveen-Selvams-campaign-for-the-True-Wanderer-contest%2F114038852016307&amp;send=false&amp;layout=standard&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font=arial&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>
+    </div>
 </div>
