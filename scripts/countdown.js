d06d20ca6b7226e35a72677653a8b902c39156c5
Praveen Selvam <praveen.selvam@gmail.com>
2011-06-01 21:26:16 +0530

    Adding var to IE.

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -17,27 +17,28 @@ function timer(){
         return;
     }
 	countdown();
-	setInterval("countdown()", 13);
 }
 
+var interval = 13;
+
 function countdown(){
-    counter+=13;
+    counter+=interval;
 	var d=new Date();
 	var cur_ts=d.getTime();
 	cur_ts=Math.floor(cur_ts/1000);
 	if(cur_ts < target_ts){
 	    
-		diff=target_ts-cur_ts;
-		days=diff/day;
+		diff=target_ts-cur_ts * 1;
+		var days=diff/day;
 		int_days=Math.floor(days);
-		diff=diff%day;
-		hours=diff/hr;
+        diff=diff%day;
+		var hours=diff/hr;
 		int_hrs=Math.floor(hours);
 		diff=diff%hr;
-		mins=diff/min;
+		var mins=diff/min;
 		int_min=Math.floor(mins);
 		diff=diff%min;
-		secs=diff;
+		var secs=diff;
 		
 		var a = document.getElementById("_countdown_");
 		days_div.innerHTML = int_days + '';
@@ -45,8 +46,10 @@ function countdown(){
 		mins_div.innerHTML = int_min + '';
 		secs_div.innerHTML = secs + '';
 		msecs_div.innerHTML = (1000 - (counter % 1000)) + '';
+		setTimeout(function(){
+		    countdown()
+		}, interval);
 	}else{
 		clearInterval();
-		//document.location="http://touring.praveenselvam.com"
 	}
 }
